ed73b7d48dd7e955e9ccaeac41cd5dad4872051f
Hilman Fauzi Herdiana <kk726627@gmail.com>
2022-03-07 17:20:26 +0700

    Hilman commit

--- 2000879.php
+++
@@ -1,8 +0,0 @@
-<?php
-$nama = "Hilman Fauzi Herdiana";
-$nim  = "2000879";
-$kelas = "C12020";
-echo "Nama : " . $nama . "</br>";
-echo "NIM : " . $nim . "</br>";
-echo "Kelas : " . $kelas . "</br>";
-?>
