5d9ffdcbe685af5ad76977fc50771e45d78e72be
Axel Eldrian <91566708+eldrians@users.noreply.github.com>
2022-03-07 19:28:54 +0700

    Axel commit

---
+++ 2000352_AxelEldrianHadiwibowo_C12020.php
@@ -0,0 +1,9 @@
+<?php
+    $Name = "Axel Eldrian Hadiwibowo";
+    $NIM = "2000352";
+    $Kelas = "Ilmu Komputer C1";
+
+    echo "Nama : ". $Name."<br>";
+    echo "NIM : ". $NIM."<br>";
+    echo "Kelas : ". $Kelas."<br>";
+?>
