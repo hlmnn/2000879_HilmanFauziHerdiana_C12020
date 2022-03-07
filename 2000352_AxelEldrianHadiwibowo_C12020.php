da216f4607f60b3c45086e1e8797fc8203fd250e
Axel Eldrian <91566708+eldrians@users.noreply.github.com>
2022-03-07 18:21:37 +0700

    Delete 2000352_AxelEldrianHadiwibowo_C12020.php

--- 2000352_AxelEldrianHadiwibowo_C12020.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $Name = "Axel Eldrian Hadiwibowo";
-    $NIM = "2000352";
-    $Kelas = "Ilmu Komputer C1";
-
-    echo "Nama : ". $Name."<br>";
-    echo "NIM : ". $NIM."<br>";
-    echo "Kelas : ". $Kelas."<br>";
-?>
